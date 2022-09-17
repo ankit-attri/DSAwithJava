f18885c39f3755d1b97db2a2a61e6e93e356d7fb
Ankit Attri <attriankit19@gmail.com>
2022-09-17 09:32:53 +0530

    added array folder

---
+++ Arrays/PrintArrays.java
@@ -0,0 +1,9 @@
+class Solution{
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        System.out.println("The array elements are : -->");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
